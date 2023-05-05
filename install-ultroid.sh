7e8ffda28a803a546db1acf480e3485d35b9d018
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-05-05 11:16:30 +0530

    Add files via upload

--- install-ultroid.sh
+++ install-ultroid.sh
@@ -1,3 +1,46 @@
+cenv() {
+touch .env
+random
+echo "Creating .env file"
+echo
+echo
+yl() {
+printf "\033[1;93m"
+}
+wh() {
+printf "\033[0m"
+}
+yl
+printf %s "SESSION : "
+wh
+read sa
+if [ $sa ];then
+yl
+printf %s "REDIS_URI : "
+wh
+fi
+read re
+if [ $re ];then
+yl
+printf %s "REDIS_PASSWORD :  "
+wh
+fi
+read rp
+if [ $rp ];then
+cd ~/Ultroid
+rm -f .env > /dev/null 2>&1
+cat >> .env << EOF
+API_ID=3704772
+API_HASH=b8e50a035abb851c0dd424e14cac4c06
+SESSION=$sa
+REDIS_URI=$re
+REDIS_PASSWORD=$rp
+EOF
+chmod 700 .env
+chmod -R 700 *
+chmod -R 700 .*
+fi
+}
 ab() {
 if [ -e Termux-Ultroid ];then
 cd Termux*
@@ -5,8 +48,8 @@ cp random /bin
 chmod 777 /bin/random
 bash rr*
 cd ~/Ultroid
-printf "\n\n press Enter to edit \033[1;92m .Env\n"
-nano .env
+printf "\n\n press Enter to fill .env \033[1;92m .Env\n"
+cenv
 else
 printf "\n\033[1;92m First clone Termux-Ultroid\n"
 fi
