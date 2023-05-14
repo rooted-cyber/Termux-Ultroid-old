88fa1625af2ce8e9cf8a1783e76d0430c5bc1acf
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-05-14 13:29:01 +0530

    Update install-ubuntu.sh

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -2,4 +2,9 @@ apt update
 apt upgrade
 apt install wget proot python
 wget https://raw.githubusercontent.com/rooted-cyber/Anlinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh
-bash ubuntu.sh
+cat >> $PREFIX/bin << EOF
+#!/data/data/com.termux/files/usr/bin/bash
+cd ~/Te*Ult*
+bash sta*ubu*
+EOF
+print "\n \033[1;92m Now you can star ubuntu Type:\033[1;93m sub\n"
