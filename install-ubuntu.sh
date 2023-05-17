a173bb36aa7c0a8e00e9246b31c6f128ec7c7b34
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-05-17 09:59:01 +0530

    Update install-ubuntu.sh

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -2,10 +2,11 @@ apt update
 apt upgrade
 apt install wget proot python
 wget https://raw.githubusercontent.com/rooted-cyber/Anlinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh
-cat >> $PREFIX/bin << EOF
+cat >> $PREFIX/bin/sub << EOF
 #!/data/data/com.termux/files/usr/bin/bash
 cd ~/Te*Ult*
 bash sta*ubu*
 EOF
+chmod 777 $PREFIX/bin/sub
 bash ubuntu.sh
 printf "\n \033[1;92m Now you can star ubuntu Type:\033[1;93m sub\n"
