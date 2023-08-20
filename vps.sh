2429a52ca5589ed8db234bb11d0e23b295d3d4dd
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-08-21 02:31:40 +0530

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -1,8 +1,8 @@
 printf "\n \033[1;92m Installing Ultroid\n"
 cd ~
-apt update
-apt upgrade
-apt install python3 python3-pip wget git nano screen
+sudo apt update
+sudo apt upgrade
+sudo apt install python3 python3-pip wget git nano screen
 python3 -m pip install --upgrade pip
 git clone https://github.com/TeamUltroid/Ultroid
 cd Ultroid
